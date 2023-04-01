665524f70da9ebf1ea9893e587bb1dbf5cbd97c4
Omanshu Aman <omanshuaman@gmail.com>
2023-04-01 12:01:37 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Divider from "../assests/divider.svg";
+import { useHistory, useLocation } from "react-router-dom";
+const NavBar = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const nextPage = () => {
+    history.push("/viewApplicants");
+  };
+  const jobPage = () => {
+    history.push("/jobPosts");
+  };
+  const assignPage = () => {
+    history.push("/assignRoles");
+  };
+  return (
+    <div className="navbar">
+      <div
+        className={`tab-name ${
+          location.pathname === "/assignRoles" ? "selected" : ""
+        }`}
+        onClick={() => assignPage()}
+      >
+        Assign Roles
+      </div>
+      <img src={Divider} alt="Your SVG" className="divider-svg" />
+      <div
+        className={`tab-name ${
+          location.pathname === "/postJob" ? "selected" : ""
+        }`}
+        onClick={() => jobPage()}
+      >
+        Post Job
+      </div>
+      <img src={Divider} alt="Your SVG" className="divider-svg" />
+      <div
+        className={`applicant-name ${
+          location.pathname === "/viewApplicantsInfo" ? "selected" : ""
+        }`}
+        onClick={() => nextPage()}
+      >
+        View Applicants Info
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/NavNheader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+const NavNheader = () => {
+  return (
+    <div>
+      <Header />
+      <NavBar />
+    </div>
+  );
+};
+
+export default NavNheader;
